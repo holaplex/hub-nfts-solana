3289a60afbd27ce2717cde7fd87fc1f58ee68d00
Kyle Espinola <kyle.s.espinola@gmail.com>
2023-08-03 13:37:39 +0200

    refactor: swap to passing database connection to service objects for doing queries

--- core/src/collection_mints.rs
+++ core/src/collection_mints.rs
@@ -1,45 +1,38 @@
 use holaplex_hub_nfts_solana_entity::{
-    collection_mints::{ActiveModel, Column, Entity, Model, Relation},
+    collection_mints::{ActiveModel, Column, Entity, Model},
     collections,
 };
-use sea_orm::{prelude::*, JoinType, QuerySelect, Set};
-
-use crate::db::Connection;
+use sea_orm::{prelude::*, Set};
 
 pub struct CollectionMint;
 
 impl CollectionMint {
-    pub async fn create(db: &Connection, model: Model) -> Result<Model, DbErr> {
-        let conn = db.get();
-
+    pub async fn create(conn: &DatabaseConnection, model: Model) -> Result<Model, DbErr> {
         let active_model: ActiveModel = model.into();
 
         active_model.insert(conn).await
     }
 
-    pub async fn find_by_id(db: &Connection, id: Uuid) -> Result<Option<Model>, DbErr> {
-        let conn = db.get();
-
+    pub async fn find_by_id(conn: &DatabaseConnection, id: Uuid) -> Result<Option<Model>, DbErr> {
         Entity::find().filter(Column::Id.eq(id)).one(conn).await
     }
 
     pub async fn update_owner_and_ata(
-        db: &Connection,
+        conn: &DatabaseConnection,
         model: &Model,
         owner: String,
         ata: String,
     ) -> Result<Model, DbErr> {
-        let conn = db.get();
-
         let mut active_model: ActiveModel = model.clone().into();
         active_model.owner = Set(owner);
         active_model.associated_token_account = Set(ata);
         active_model.update(conn).await
     }
 
-    pub async fn find_by_ata(db: &Connection, ata: String) -> Result<Option<Model>, DbErr> {
-        let conn = db.get();
-
+    pub async fn find_by_ata(
+        conn: &DatabaseConnection,
+        ata: String,
+    ) -> Result<Option<Model>, DbErr> {
         Entity::find()
             .filter(Column::AssociatedTokenAccount.eq(ata))
             .one(conn)
@@ -47,11 +40,9 @@ impl CollectionMint {
     }
 
     pub async fn find_by_id_with_collection(
-        db: &Connection,
+        conn: &DatabaseConnection,
         id: Uuid,
     ) -> Result<Option<(Model, Option<collections::Model>)>, DbErr> {
-        let conn = db.get();
-
         Entity::find()
             .find_also_related(collections::Entity)
             .filter(Column::Id.eq(id))
@@ -59,9 +50,7 @@ impl CollectionMint {
             .await
     }
 
-    pub async fn update(db: &Connection, model: ActiveModel) -> Result<Model, DbErr> {
-        let conn = db.get();
-
+    pub async fn update(conn: &DatabaseConnection, model: ActiveModel) -> Result<Model, DbErr> {
         model.update(conn).await
     }
 }
--- core/src/compression_leafs.rs
+++ core/src/compression_leafs.rs
@@ -1,40 +1,30 @@
 use holaplex_hub_nfts_solana_entity::compression_leafs::{ActiveModel, Column, Entity, Model};
 use sea_orm::prelude::*;
 
-use crate::db::Connection;
-
 pub struct CompressionLeaf;
 
 impl CompressionLeaf {
-    pub async fn create(db: &Connection, model: Model) -> Result<Model, DbErr> {
-        let conn = db.get();
-
+    pub async fn create(conn: &DatabaseConnection, model: Model) -> Result<Model, DbErr> {
         let active_model: ActiveModel = model.into();
 
         active_model.insert(conn).await
     }
 
-    pub async fn find_by_id(db: &Connection, id: Uuid) -> Result<Option<Model>, DbErr> {
-        let conn = db.get();
-
+    pub async fn find_by_id(conn: &DatabaseConnection, id: Uuid) -> Result<Option<Model>, DbErr> {
         Entity::find().filter(Column::Id.eq(id)).one(conn).await
     }
 
     pub async fn find_by_asset_id(
-        db: &Connection,
+        conn: &DatabaseConnection,
         address: String,
     ) -> Result<Option<Model>, DbErr> {
-        let conn = db.get();
-
         Entity::find()
             .filter(Column::AssetId.eq(address))
             .one(conn)
             .await
     }
 
-    pub async fn update(db: &Connection, model: ActiveModel) -> Result<Model, DbErr> {
-        let conn = db.get();
-
+    pub async fn update(conn: &DatabaseConnection, model: ActiveModel) -> Result<Model, DbErr> {
         model.update(conn).await
     }
 }
