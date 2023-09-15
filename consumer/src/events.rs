7dfae56fea0e69554af1247e3a4c1648ebe340f5
mpw <x@mpw.sh>
2023-09-14 22:51:32 -0300

    update toolchain, add metrics to mint_to_collection

--- consumer/src/events.rs
+++ consumer/src/events.rs
@@ -868,6 +868,7 @@ impl Processor {
         key: &SolanaNftEventKey,
         payload: MintMetaplexMetadataTransaction,
     ) -> ProcessResult<SolanaPendingTransaction> {
+        let start = Instant::now();
         let conn = self.db.get();
         let id = Uuid::parse_str(&key.id.clone())?;
         let collection_id = Uuid::parse_str(&payload.collection_id)?;
@@ -894,6 +895,12 @@ impl Processor {
             };
 
             CompressionLeaf::create(conn, compression_leaf).await?;
+            let elapsed = i64::try_from(start.elapsed().as_millis()).unwrap_or(0);
+
+            self.metrics.rpc_tx_duration_ms_bucket.record(elapsed, &[
+                KeyValue::new("blockchain", "Solana"),
+                KeyValue::new("compressed", "true"),
+            ]);
 
             return Ok(tx.into());
         }
@@ -914,6 +921,12 @@ impl Processor {
         };
 
         CollectionMint::create(conn, collection_mint).await?;
+        let elapsed = i64::try_from(start.elapsed().as_millis()).unwrap_or(0);
+
+        self.metrics.rpc_tx_duration_ms_bucket.record(elapsed, &[
+            KeyValue::new("blockchain", "Solana"),
+            KeyValue::new("compressed", "false"),
+        ]);
 
         Ok(tx.into())
     }
