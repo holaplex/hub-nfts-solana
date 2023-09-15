92ff079483ed0e8e4f1f2af7b75576905f4cf0ab
mpw <x@mpw.sh>
2023-09-15 14:01:37 -0300

    cargo fmt

--- consumer/src/events.rs
+++ consumer/src/events.rs
@@ -904,13 +904,12 @@ impl Processor {
             CompressionLeaf::create(conn, compression_leaf).await?;
             let elapsed = i64::try_from(start.elapsed().as_millis()).unwrap_or(0);
 
-            self.metrics.rpc_tx_assembly_duration_ms_bucket.record(
-                elapsed,
-                &[
+            self.metrics
+                .rpc_tx_assembly_duration_ms_bucket
+                .record(elapsed, &[
                     KeyValue::new("blockchain", "Solana"),
                     KeyValue::new("compressed", "true"),
-                ],
-            );
+                ]);
 
             return Ok(tx.into());
         }
@@ -933,13 +932,12 @@ impl Processor {
         CollectionMint::create(conn, collection_mint).await?;
         let elapsed = i64::try_from(start.elapsed().as_millis()).unwrap_or(0);
 
-        self.metrics.rpc_tx_assembly_duration_ms_bucket.record(
-            elapsed,
-            &[
+        self.metrics
+            .rpc_tx_assembly_duration_ms_bucket
+            .record(elapsed, &[
                 KeyValue::new("blockchain", "Solana"),
                 KeyValue::new("compressed", "false"),
-            ],
-        );
+            ]);
 
         Ok(tx.into())
     }
